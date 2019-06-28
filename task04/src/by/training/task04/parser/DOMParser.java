2fd140d24a431a12228e0a34ec65323338da6049
MarkGolubtsov <glmarkgl4@gmail.com>
2019-06-28 21:49:58 +0300

    Parsers

--- task04/src/by/training/task04/parser/DOMParser.java
+++ task04/src/by/training/task04/parser/DOMParser.java
@@ -17,25 +17,25 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DOMCardParser implements XmlParser<Flower> {
+public class DOMParser implements XmlParser<Flower> {
 
-    private static DOMCardParser ourInstance;
+    private static DOMParser ourInstance;
     private DocumentBuilder documentBuilder;
 
     static {
       ///
         try {
-            ourInstance = new DOMCardParser();
+            ourInstance = new DOMParser();
         } catch (ParserException e) {
             e.printStackTrace();
         }
 
     }
-    public static DOMCardParser getInstance() {
+    public static DOMParser getInstance() {
         return ourInstance;
     }
 
-    private DOMCardParser() throws ParserException {
+    private DOMParser() throws ParserException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         try {
             documentBuilder = factory.newDocumentBuilder();
@@ -55,8 +55,8 @@ public class DOMCardParser implements XmlParser<Flower> {
             //logger.debug("Start DOM parsing");
             for (int i = 0; i < flowerList.getLength(); i++) {
                 Element flowerElement = (Element) flowerList.item(i);
-                Flower card = buildFlower(flowerElement);
-                flowers.add(card);
+                Flower flower = buildFlower(flowerElement);
+                flowers.add(flower);
             }
         } catch (SAXException | IOException e) {
             throw new ParserException();
@@ -88,11 +88,11 @@ public class DOMCardParser implements XmlParser<Flower> {
         NodeList nList = element.getElementsByTagName(elementName);
         Node node = nList.item(0);
         String text = node.getTextContent();
-        //logger.debug("Add to card " + text);
+        //logger.debug("Add to  " + text);
         return text;
     }
         public static void main(String[] args) throws URISyntaxException, ParserException {
-        DOMCardParser domParser = DOMCardParser.getInstance();
+        DOMParser domParser = DOMParser.getInstance();
         List<Flower> post = domParser.getData("D:\\EPAM_JAVA\\TASK\\task04\\src\\by\\training\\task04\\data\\sxema.xml");
         post.forEach(System.out::println);
     }
